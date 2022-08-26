5ec7d73a465902190d87972e435ba49c7b772dad
Nicholas Zehr <nicholas.zehr@icloud.com>
2022-08-26 11:51:54 -0500

    Re-Updating config

--- backend/db/client.js
+++ backend/db/client.js
@@ -5,19 +5,11 @@ const DB_NAME = 'graceShopper-dev';
 const DB_URL =
   process.env.DATABASE_URL || `postgres://localhost:5432/${DB_NAME}`;
 
-let client;
-
-if (process.env.CI) {
-  client = new Client({
-    host: 'localhost',
-    port: 5432,
-    user: 'postgres',
-    password: 'postgres',
-    database: 'postgres',
+  const client = new Client({
+    connectionString,
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
   });
-} else {
-  // local / heroku client config
-  client = new Client(DB_URL);
-}
+  
+client.connect()
 
 module.exports = client;
